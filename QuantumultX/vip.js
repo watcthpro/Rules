b298caadf10195b542ca7eb4a4caf145b8221be9
Mr.邓 <cbwfnhxk407@gmail.com>
2020-05-01 13:56:53 +0800

    Update vip.js

--- QuantumultX/vip.js
+++ QuantumultX/vip.js
@@ -1,11 +1,51 @@
-/**
- * @fileoverview Template to compose HTTP reqeuest.
- * 
- */
+var obj = JSON.parse($response.body);
 
-var body = $response.body;
-var url = $request.url;
-var obj = JSON.parse(body);
+var id = obj.userid;
+
+obj = {
+  "code" : 0,
+  "message" : "成功",
+  "data" : {
+    "cards" : {
+      "vip" : {
+        "name" : "极速会员卡",
+        "start_time" : "2022-05-01 11:38:22",
+        "end_time" : "2022-05-08 11:38:22"
+      }
+    },
+    "equity" : [
+      {
+        "equityId" : "21",
+        "endTime" : "2022-05-08 11:38:22",
+        "equityType" : "1",
+        "name" : "免广告"
+      },
+      {
+        "equityId" : "22",
+        "endTime" : "2022-05-08 11:38:22",
+        "equityType" : "2",
+        "name" : "音量键翻页"
+      },
+      {
+        "equityId" : "23",
+        "endTime" : "2022-05-08 11:38:22",
+        "equityType" : "3",
+        "name" : "听书"
+      },
+      {
+        "equityId" : "24",
+        "endTime" : "2022-05-08 11:38:22",
+        "equityType" : "4",
+        "name" : "缓存"
+      },
+      {
+        "equityId" : "25",
+        "endTime" : "2022-05-08 11:38:22",
+        "equityType" : "7",
+        "name" : "尊贵标识"
+      }
+    ],
+    "freeAd" : {
       "ADCodeReadTopPop" : "1",
       "ADCodeReadInsertVideo" : "1",
       "ADCodeReadTopVideoButton" : "1",
@@ -27,6 +67,21 @@ var obj = JSON.parse(body);
       "ADCodeReadInsert" : "1",
       "ADCodeNewEndVideo" : "1",
       "ADCodeReadEndShadow" : "1"
-    }
+    },
+    "is_vip" : "1",
+    "userId" : "1052099891",
+    "special_desc" : "听书权益全新上线",
+    "card_id" : "1000001052099891",
+    "packages" : "0",
+    "vip_end_time" : "2022-05-08 11:38:22",
+    "icon" : {
+      "vip" : "https://img.midukanshu.com/browser/banner/2c52efc076f922d75d8bd6dc753c83c0.png"
+    },
+    "name" : "VIP会员"
+  },
+  "currentTime" : 1588308864
+};
 
-$done({body});
+obj.userid = id;
+
+$done({ body: JSON.stringify(obj) });
