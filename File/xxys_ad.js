0f068916a2f8fbb6fc14b53ad133a5d23d738749
Mr.邓 <cbwfnhxk407@gmail.com>
2020-07-25 14:28:10 +0800

    Update xxys_ad.js

--- File/xxys_ad.js
+++ File/xxys_ad.js
@@ -1,12 +1,5 @@
-/*
-小影视去ads
-MITM = *.leyiapps.com
-*/
-
 const ad = 'getGlobalData';
-
-let obj = JSON.parse($response.body);
-
+var obj = JSON.parse($response.body);
 if ($request.url.indexOf(ad) != -1) {
 delete obj.data.adrows
 delete obj.data.adgroups
