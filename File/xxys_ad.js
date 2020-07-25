5eb4bc9b96e09201fbfedbea28784b79c5998e06
Mr.邓 <cbwfnhxk407@gmail.com>
2020-07-25 23:40:32 +0800

    Update xxys_ad.js

--- File/xxys_ad.js
+++ File/xxys_ad.js
@@ -7,5 +7,7 @@ if ($request.url.indexOf(ad) != -1) {
 delete obj.data.adrows
 delete obj.data.adgroups
 delete obj.data.sdkrows_iOS
+delete obj.data.iOS_adgroups
+delete obj.data.popuptext_iOS
 }
 $done({body: JSON.stringify(obj)});
