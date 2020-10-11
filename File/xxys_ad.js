627e81a95ffec1d675a464c5eef3dfcf73d97487
Mr.邓 <cbwfnhxk407@gmail.com>
2020-10-11 19:40:42 +0800

    Update xxys_ad.js

--- File/xxys_ad.js
+++ File/xxys_ad.js
@@ -1,18 +1,8 @@
-/*
-https:\/\/.*\/getGlobalData
-*/
-const path1 = "/ucp/index";
-const path2 = "/vod/reqplay/";
+
+const path1 = "/vod/reqplay/";
 const ad = 'getGlobalData';
 var obj = JSON.parse($response.body);
 
-if ($request.url.indexOf(path1) != -1){
-	obj.data.uinfo["down_daily_remainders"] = "666";
-	obj.data.uinfo["play_daily_remainders"] = "666";
-	obj.data.uinfo["curr_group"] = "5";
-	obj.data.user["isvip"] = "1";
-	obj.data.user["goldcoin"] = "666";
-}
 if ($request.url.indexOf(path2) != -1){
 	obj.retcode = "0";
 	obj.data.lastplayindex = "1";
