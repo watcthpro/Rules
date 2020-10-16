41ea7dcac3e76b06d3db00347c178fd98a18d299
Mr.邓 <cbwfnhxk407@gmail.com>
2020-10-16 21:05:34 +0800

    Create 人人视频_ad.js

---
+++ File/人人视频_ad.js
@@ -0,0 +1,13 @@
+/*
+https:\/\/api\.rr\.tv\/ad\/getAll
+*/
+var body = $response.body;
+var url = $request.url;
+
+if (url.indexOf('/ad/getAll') != -1) {
+	var obj = JSON.parse(body);
+	obj.data.adList = [];
+	body = JSON.stringify(obj);
+}
+
+$done({ body: JSON.stringify(obj) });
