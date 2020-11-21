f4f322f4f7270bcb087082db6643d7b60fad60c3
Mr.邓 <cbwfnhxk407@gmail.com>
2020-11-21 13:47:45 +0800

    Update xxys_ad.js

--- File/xxys_ad.js
+++ File/xxys_ad.js
@@ -12,5 +12,6 @@ delete obj.data.iOS_adgroups
 delete obj.data.sdkrows_iOS
 delete obj.data.sdkrows_Android
 delete obj.data.Android_adgroups
+delete obj.data.popuptext_iOS
 
 $done({body: JSON.stringify(obj)});
