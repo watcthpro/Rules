e6fa2df743fd2b9db0b479bb1ded3e96bfe6ccce
Mr.邓 <cbwfnhxk407@gmail.com>
2021-02-15 22:07:04 +0800

    Update xxys.js

--- File/xxys.js
+++ File/xxys.js
@@ -9,12 +9,7 @@ delete obj.data.pmap
 if ($request.url.indexOf(adold) != -1) {
 delete obj.data.adrows
 delete obj.data.adgroups
-delete obj.data.iOS_adgroups
-delete obj.data.global_adgroup_ad4
-delete obj.data.global_adgroup_ad8
-delete obj.data.global_adgroup_ad16
-delete obj.global_adgroup_ad1
-  
+delete obj.data.iOS_adgroups  
 }
 
 $done({body: JSON.stringify(obj)});
