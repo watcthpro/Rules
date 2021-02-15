bc2f6840565f3f59d90d21f63eaea888adacea8d
Mr.邓 <cbwfnhxk407@gmail.com>
2021-02-15 20:45:54 +0800

    Update xxys.js

--- File/xxys.js
+++ File/xxys.js
@@ -10,6 +10,8 @@ if ($request.url.indexOf(adold) != -1) {
 delete obj.data.adrows
 delete obj.data.adgroups
 delete obj.data.iOS_adgroups
+delete obj.data.global_adgroup
+
 }
 
 $done({body: JSON.stringify(obj)});
