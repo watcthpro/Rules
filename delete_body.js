b27f3271f4b4e49aa5f723453c756f9ef1fc7400
Mr.邓 <cbwfnhxk407@gmail.com>
2021-08-21 17:04:00 +0800

    Update delete_body.js

--- delete_body.js
+++ delete_body.js
@@ -1,5 +1,7 @@
+/*
+^https:\/\/api\.ngayet\.com\/Home\/cyhb_index.* url script-response-body delete_body.js
+*/
 var obj = JSON.parse($response.body);
 delete obj.body.model_list_two;
-delete obj.body.adv_middle;
-delete obj.body.system_app_id;
+delete obj.body.adv_bottom.param_img;
 $done({body: JSON.stringify(obj)});
