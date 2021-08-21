5f718af684278d507b0e8a01ef8ddf19c3088ef9
Mr.邓 <cbwfnhxk407@gmail.com>
2021-08-21 16:26:12 +0800

    Update delete_body.js

--- delete_body.js
+++ delete_body.js
@@ -1,3 +1,6 @@
 var obj = JSON.parse($response.body);
 delete obj.body.model_list_two;
+delete obj.body.adv_bottom;
+delete obj.body.adv_middle;
+delete obj.body.system_app_id;
 $done({body: JSON.stringify(obj)});
