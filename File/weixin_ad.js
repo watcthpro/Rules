f0907dc89d3cfdbb67835b0e1e9ae4d479e6ed1e
watcthpro <cbwfnhxk407@gmail.com>
2021-08-23 22:13:56 +0800

    weixin.2

--- File/weixin_ad.js
+++ File/weixin_ad.js
@@ -1,3 +1,8 @@
+/*
+^https?:\/\/mp\.weixin\.qq\.com\/mp\/(?>ad_|advertisement|getappmsgad)
+*/
 var obj = JSON.parse($response.body);
 delete obj.advertisement_info;
+delete obj.appid;
+advertisement_num=0;
 $done({body: JSON.stringify(obj)});
