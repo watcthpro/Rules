65d5974f8f9d4ccde92713d3c24abaf1f580100c
watcthpro <cbwfnhxk407@gmail.com>
2021-08-23 21:54:11 +0800

    weixin

---
+++ File/weixin_ad.js
@@ -0,0 +1,13 @@
+let resp = $response.body;
+try {
+    resp = JSON.parse(resp);
+    resp.advertisement_num=0;
+    resp.advertisement_info=[];
+    delete resp.appid;
+    delete advertisement_info;
+    resp = JSON.stringify(resp);
+} catch (e) {
+    console.log(`weixin ad error:\n${e.message}`);
+}
+
+$done({body: resp});
