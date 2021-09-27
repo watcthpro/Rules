9dc668003a3fa242c4b1e39cd3f6582b6621832f
Mr.邓 <cbwfnhxk407@gmail.com>
2021-09-27 09:23:57 +0800

    Delete delete cyhb_index.js

--- File/delete cyhb_index.js
+++
@@ -1,7 +0,0 @@
-/*
-^https:\/\/api\.ngayet\.com\/Home\/cyhb_index.* url script-response-body delete_body.js
-*/
-var obj = JSON.parse($response.body);
-delete obj.body.model_list_two;
-delete obj.body.adv_bottom.param_img;
-$done({body: JSON.stringify(obj)});
