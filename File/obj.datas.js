2381b08844a795bdf3757c491c54b40e85b3986f
Mr.邓 <cbwfnhxk407@gmail.com>
2022-01-27 18:57:17 +0800

    Update obj.datas.js

--- File/obj.datas.js
+++ File/obj.datas.js
@@ -1,3 +1,7 @@
-var obj = JSON.parse($response.body);
-delete obj.response.datas;
+const ad = 'smkoperate/bid/1.0.0/getBidResource';
+let obj = JSON.parse($response.body);
+
+if ($request.url.indexOf(ad) != -1) {
+delete obj.response.datas
+}
 $done({body: JSON.stringify(obj)});
